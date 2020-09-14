d479f646ab1a4f5d0245b19110be1b0c30b4bc73
Maruf Hassan <mrfhassan17@gmail.com>
2020-09-14 14:10:06 +0530

    Added left and right cards

--- lib/route.dart
+++ lib/route.dart
@@ -28,32 +28,40 @@ class _RoutePageState extends State<RoutePage> {
                 duration: const Duration(milliseconds: 400),
                 left: centerBus,
                 top: animated ? 15 : constraints.maxHeight - _busSize - 10,
-                bottom: 0,
+                bottom: 0.0,
                 child: BusLine(),
               ),
+
+              /// 1st Dot
               AnimatedPositioned(
                 duration: const Duration(milliseconds: 600),
                 left: centerDot,
                 top: animated ? 100 : constraints.maxHeight,
-                child: RouteDot(),
+                child: RouteDot(selected: true),
               ),
+
+              /// 2nd Dot
               AnimatedPositioned(
                 duration: const Duration(milliseconds: 800),
-                left: centerDot,
+                right: centerDot,
                 top: animated ? 200 : constraints.maxHeight,
-                child: RouteDot(),
+                child: RouteDot(selected: true, left: true),
               ),
+
+              /// 3rd Dot
               AnimatedPositioned(
                 duration: const Duration(milliseconds: 1000),
                 left: centerDot,
                 top: animated ? 300 : constraints.maxHeight,
                 child: RouteDot(),
               ),
+
+              /// 4th Dot
               AnimatedPositioned(
                 duration: const Duration(milliseconds: 1200),
-                left: centerDot,
+                right: centerDot,
                 top: animated ? 400 : constraints.maxHeight,
-                child: RouteDot(),
+                child: RouteDot(selected: true, left: true),
               ),
               Align(
                 alignment: Alignment.bottomCenter,
@@ -99,26 +107,65 @@ class BusLine extends StatelessWidget {
 
 class RouteDot extends StatelessWidget {
   final bool selected;
+  final bool displayCard;
+  final bool left;
 
-  const RouteDot({this.selected = false});
+  const RouteDot({
+    this.selected = false,
+    this.displayCard = false,
+    this.left = false,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: _dotSize,
-      width: _dotSize,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        border: Border.all(
-          color: Colors.grey[100],
+    return Row(
+      children: <Widget>[
+        if (left) ...[
+          Container(
+            color: Colors.grey[200],
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text('JFK + ORY'),
+            ),
+          ),
+          Container(
+            width: 10,
+            height: 1,
+            color: Colors.grey[400],
+          ),
+        ],
+        Container(
+          height: _dotSize,
+          width: _dotSize,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            border: Border.all(
+              color: Colors.grey[100],
+            ),
+            shape: BoxShape.circle,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: CircleAvatar(
+              backgroundColor: selected ? Colors.red : Colors.green,
+            ),
+          ),
         ),
-        shape: BoxShape.circle,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(3.0),
-        child: CircleAvatar(
-          backgroundColor: selected ? Colors.red : Colors.green,
-        ),
-      ),
+        if (!left) ...[
+          Container(
+            color: Colors.grey[200],
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text('JFK + ORY'),
+            ),
+          ),
+          Container(
+            width: 10,
+            height: 1,
+            color: Colors.grey[400],
+          ),
+        ],
+      ],
     );
   }
 }
